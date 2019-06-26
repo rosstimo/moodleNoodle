c8df69966ddd176a15fb012d0f638027e2ae8c6d
Tim Rossiter <rosstimo@isu.edu>
2019-06-25 20:27:41 -0600

    Update ClozeQ.py
    test and show function  added

--- ClozeQ.py
+++ ClozeQ.py
@@ -79,45 +79,20 @@ class ClozeQ():
 
 
 
-    def __init__(self, questionPoints, questionType, acceptedError, responses=None):
+    def __init__(self, questionPoints, questionType, responses=None):
         self.questionPoints = questionPoints
         self.questionType = questionType
         if responses is None:
             self.responses = []
         else:
             self.responses = responses
-        self.acceptedError = acceptedError
-
 
     @property
     def clozeStr(self):
         responsStr=''
         for item in reponsees:
-            responsStr += responses(str(item))+':' + str(self.acceptedError)
-
-        return '{}:{}:{}{}'.format(self.questionPoints, self.questionType)
-
-test = ClozeQ(1,'NM', 1,('%100%',4) )
-
-# some test cases
-myAnswers = ('=Some Answer#feedback', '%50%half right#a little feedback', '=4:1#close enough', '%100%4:1', 'wrong#close enough', '~nope#try again~=yep#you got it!')
-str_1 = '{1:MULTICHOICE:%100%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%0%Answer 3#Feedback 3}'
-str_2 = '{1:SHORTANSWER:%100%blank#Feedback 1~%100%space#Feedback 2}'
-str_3 = '{1:NUMERICAL:=0.6667:0.0001#Feedback for correct answer + or - .0001 ~%50%0.6667:.01#Feedback for half credit near correct answer in this case too much rounding error}'
-str_4 = '{1:NM:=4:1}'
-str_5 = '{1:MULTICHOICE:=Answer 1#Feedback 1~Answer 2#Feedback 2~Answer 3#Feedback 3}'
-str_6 = '{1:MULTICHOICE:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}'
-str_7 = '{:MRS:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}'
-str_8 = '{:MRS:%50%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%-50%Answer 3#Feedback 3~%-50%Answer 4#Feedback 4}'
-
-str_10 = '''{1:MULTICHOICE:%100%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%0%Answer 3#Feedback 3}'+
-'{1:SHORTANSWER:%100%blank#Feedback 1~%100%space#Feedback 2}'
-'{1:NUMERICAL:=0.6667:0.0001#Feedback for correct answer + or - .0001 ~%50%0.6667:.01#Feedback for half credit near correct answer in this case too much rounding error}' +
-'{1:NM:=4:1}'+
-'{1:MULTICHOICE:=Answer 1#Feedback 1~Answer 2#Feedback 2~Answer 3#Feedback 3}'+
-'{1:MULTICHOICE:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}'+
-'{:MRS:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}'+
-'{:MRS:%50%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%-50%Answer 3#Feedback 3~%-50%Answer 4#Feedback 4}'''
+            responsStr += responseStr('%' + item[0] + '%' + item[1] + '#' + item[2])
+        return '{{}:{}:{}}'.format(self.questionPoints, self.questionType, responsStr)
 
 def getQuestions(clozeStr):
     '''returns list of all cloze formattes questions in a string'''
@@ -133,15 +108,6 @@ def getQPoints(clozeStr):
 
 def getQType(clozeStr):
     '''returns Moodle Cloze question type code as a string'''
-    # flag = False
-    #qType = ''
-    # for char in clozeStr:
-    #     if char == ':' :
-    #         flag = not flag
-    #     elif char != ':' and flag == True:
-    #         qType += char
-    #     elif char and flag == True:
-    #         break
     pattern = re.compile(r':\w+:')
     list_matches = pattern.findall(clozeStr)
     return list_matches[0].replace(':','')#TODO: see if there is a cleanear way to return what is between the :'s only
@@ -154,17 +120,14 @@ def getQResponses(clozeStr):
         s = re.search(r':' + getQType(match) + ':', match)
         e = re.search(r'}', match)
         responseStr = match[s.end():e.end()-1]
-        junk = getResponse(responseStr)
-    return matches
+    return getResponse(responseStr)
 
 def getResponse(responseStr):
     ''' Takes a string of possible responses stripped from Moodle Cloze formatted string and seperates: answer points, answer, acceptable error value in a NUMERICAL or NM type, and feedback'''
-
     final = []
     responses = responseStr.split('~')
     for response in responses:
         temp = []
-        print(response)
         if response[0] == '=':
             temp.append('100')
             nut = response[1:]
@@ -175,101 +138,52 @@ def getResponse(responseStr):
         else:
             temp.append('0')
             nut = response
-        print(nut)
         if '#' in nut:
             cracked = nut.split('#')
             temp.append(cracked[0])
             temp.append(cracked[1])
         else:
             temp.append(nut)
+            temp.append('')
         final.append(temp)
-    print(final)
-    return None
+    return final
 
 def wrapQuestion():
     '''returns Moodle Cloze formatted string'''
     pass
-    #return
-clozeQuestions = getQuestions(str_10)
+
+def showQuestion(question):
+    '''prints a deconstructed view of a Moodle Cloze Question'''
+    print('')
+    print(question)
+    print('')
+    print('-- Question Points:      ' + getQPoints(question))
+    print('   Question Type:        ' + getQType(question))
+    print('   Question Responses:')
+    for q in getQResponses(question):
+        print('    -- Points:           ' + q[0])
+        print('       Response:         ' + q[1])
+        print('       Feedback:         ' + q[2])
+
+def testSample():
+    '''Returns a string of various Moodle Cloze formatted questions'''
+    sample = '''{105:MULTICHOICE:%100%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%0%Answer 3#Feedback 3}
+    {1:SHORTANSWER:%100%blank#Feedback 1~%100%space#Feedback 2}
+    {1:NUMERICAL:=0.6667:0.0001#Feedback for correct answer + or - .0001 ~%50%0.6667:.01#Feedback for half credit near correct answer in this case too much rounding error}' +
+    {1:NM:=4:1}
+    {1:MULTICHOICE:=Answer 1#Feedback 1~Answer 2#Feedback 2~Answer 3#Feedback 3}
+    {1:MULTICHOICE:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}
+    {:MRS:Answer 1#Feedback 1~=Answer 2#Feedback 2~Answer 3#Feedback 3}
+    {:MRS:%50%Answer 1#Feedback 1~%50%Answer 2#Feedback 2~%-50%Answer 3#Feedback 3~%-50%Answer 4#Feedback 4}'''
+    return sample
+
+if __name__ == '__main__' :
+    pass
+
+'''TESTING BELOW HERE'''
+
+test = ClozeQ(1,'NM', 1,('%100%',4) )
+
+clozeQuestions = getQuestions(testSample())
 for q in clozeQuestions:
-    print('')
-    print(q)
-    print('')
-    print('Question Points:    ' + getQPoints(q))
-    print('Question Type:      ' + getQType(q))
-    print('Question Responses:' , getQResponses(q))
-
-#
-# pattern = re.compile(r':\w+:')
-# matches = pattern.finditer(str_1)
-# list_matches = pattern.findall(str_1)
-# for match in matches:
-#     print(match)
-# for match in list_matches:
-#     print(match)
-# print(len(list_matches))
-# first_match = pattern.search(str_1)
-# print(first_match)
-
-'''
-#parse up possible response combinationd. assumes prior extraction from cloze formated string
-for answer in myAnswers:
-    points = ''
-    ans = ''
-    accErr = ''
-    feedback = ''
-    #junk = answer.split('=')
-    #for i in enumerate(junk):
-    #    print(i)
-    #break
-
-    if '=' in answer:
-        aList = answer.split('=')
-        points = '100'
-    elif '%100%'in answer:
-        aList = answer.split('%100%')
-        points = '100'
-    elif "%" in answer[0] and "%" in answer[2:4]:
-        aList = answer.split('%')
-    else: #TODO:I think Moodle will error if there is no 100% answer not sure. some MR or MRS might not have them. if so all blank strings here should be fine.
-        aList = ('', '', answer)  #('','$$!No right answer found!',answer)
-        #points = '0'
-
-    print(answer)
-    for i in enumerate(aList):
-        print(i)
-
-    fList = aList[1].split('#')
-    print('     ',aList)
-    for i in enumerate(fList):
-        print('     ',i)
-
-    #ans = fList[0]
-    #feedback = fList[1]
-
-    if "%" in answer[0] and "%" in answer[2:4]:
-        pList = answer.split('%')
-        points = pList[1]
-        ans = pList[2]
-    else:
-        pList = ('', '')
-        points = '0'
-
-    print(answer)
-    for i in enumerate(pList):
-        print(i)
-
-
-    if "#" in ans:
-        fList = ans.split('#')
-        feedback = fList[len(fList)-1]
-        ans = fList[0]
-    if ":" in ans:
-        aList = ans.split(':')
-        ans = aList[0]
-        accErr = aList[1]
-
-    #print('points: ' + points,' - Answer: ' + ans, ' - Accepted Error: ' + accErr, ' - FeedBack: ' + feedback)
-
-#print(test.questionType)
-'''
+    showQuestion(q)
